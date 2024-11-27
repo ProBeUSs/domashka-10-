020e483b4c292317ba354a5bbf7ca33fca54da80
Illia <podymovillia123@gmail.com>
2024-11-27 20:40:15 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 //git init
-//git add README.md
+//git add .
 //git commit -m "first commit"
-//git branch -M main
 //git remote add origin https://github.com/ProBeUSs/domashka-10-.git
 //git push -u origin main
